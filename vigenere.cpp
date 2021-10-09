d6bdb1e97d6585566e9c3bfecf2f2eb4a5f6f1f5
Ryanj-code <83621881+Ryanj-code@users.noreply.github.com>
2021-10-09 16:05:40 -0400

    Update vigenere.cpp

--- vigenere.cpp
+++ vigenere.cpp
@@ -8,6 +8,7 @@ This program contains the Vigenere Cipher functions.
 
 #include <iostream>
 #include <cctype>
+#include "vigenere.h"
 
 char up[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 char low[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
