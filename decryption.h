5047592f324fa7bf3ab0387849c3b42b5d1f83e2
Ryanj-code <83621881+Ryanj-code@users.noreply.github.com>
2021-10-09 16:05:14 -0400

    Delete decryption.h

--- decryption.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-char reverseCaesar(char c, int rshift);
-std::string decryptCaesar(std::string ciphertext, int rshift);
-char reverseVigenere(char c, std::string keyword);
-std::string decryptVigenere(std::string ciphertext, std::string keyword);
