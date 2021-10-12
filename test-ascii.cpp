11f1cf0cedeb3e6088809d7151752303e3295792
Ryanj-code <83621881+Ryanj-code@users.noreply.github.com>
2021-10-11 21:49:21 -0400

    Update test-ascii.cpp

--- test-ascii.cpp
+++ test-ascii.cpp
@@ -1,13 +1,30 @@
-#include <iostream>
+/*
+Author: Ryan Jiang
+Course: CSCI-135
+Instructor: Tong Yi and Mike Zamansky
+Assignment: Lab 6
+This contains Task A, which prints out the ascii values of a string next to each of the character.
+*/
 
-int main(){
+#include <iostream>
+#include <string>
+
+void testingAscii(std::string s){
   char c = '0';
-  std::string s = "Cat :3 Dog";
-  // Initialize char and test case.
+  // Initialize char.
   for(int i = 0; i < s.length(); i++){
     c = s[i];
     std::cout << c << " " << int(c) << "\n";
   } // Loops through s, sets c to s[i] and prints out the characters with their ASCII codes. 
-
+}
+ 
+int main(){
+  std::string test1 = "Cat :3 Dog";
+  testingAscii(test1);
+  std::cout << "\n";
+  
+  std::string test2 = "Hello World!";
+  testingAscii(test2);
+  std::cout << std::endl;
   return 0;
 }
